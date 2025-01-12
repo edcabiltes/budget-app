3871583be00775c9406004ac999fbdf11f38ba12
Ed Cabiltes <edcabiltes@outlook.com>
2025-01-12 10:02:51 +0800

    Added budget cards

--- src/helpers.js
+++ src/helpers.js
@@ -48,3 +48,31 @@ export const createExpense = ({
 export const deleteItem = ({ key }) => {
     return localStorage.removeItem(key)
 };
+
+// total spent by budget
+export const calculateSpentByBudget = (budgetId) => {
+    const expenses = fetchData("expenses") ?? [];
+    const budgetSpent = expenses.reduce((acc, expense) => {
+        if (expense.budgetId !== budgetId) return acc
+
+        return acc += expense.amount
+    }, 0);
+
+    return budgetSpent;
+};
+
+// format percentage
+export const formatPercentage = (amt) => {
+    return amt.toLocaleString(undefined, {
+        style: "percent",
+        minimumFractionDigits: 0,
+    })
+};
+
+// format currency
+export const formatCurrency = (amt) => {
+    return amt.toLocaleString(undefined, {
+        style: "currency",
+        currency: "USD"
+    })
+};
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -6,6 +6,7 @@ import { toast } from "react-toastify";
 import Intro from "../components/Intro";
 import AddBudgetForm from "../components/AddBudgetForm";
 import AddExpenseForm from "../components/AddExpenseForm";
+import BudgetItem from "../components/BudgetItem";
 
 // helper function
 import { createBudget, createExpense, fetchData, wait} from "../helpers";
@@ -75,6 +76,14 @@ const Dashboard = () => {
                                         <div className="flex-lg">
                                             <AddBudgetForm />
                                             <AddExpenseForm budgets={budgets} />
+                                        </div>
+                                        <h2>Existing Budgets</h2>
+                                        <div className="budgets">
+                                            {
+                                                budgets.map((budget) => (
+                                                    <BudgetItem key={budget.id} budget={budget} />
+                                                ))
+                                            }
                                         </div>
                                     </div>
                                 ) 
